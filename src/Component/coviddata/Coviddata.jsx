da614377dbeb252ad04514d787c6d38bbf562b7a
Kenneth Okereke <kenneth.okereke@wagner.edu>
2021-03-17 13:41:17 -0400

    covidcoop

---
+++ src/Component/coviddata/Coviddata.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Coviddata() {
+    return (
+        <div>
+            data
+        </div>
+    )
+}
+
+export default Coviddata
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Coviddata from './Component/coviddata/Coviddata'
+import Navbar from './Component/Navbar/Navbar'
+
+function Home() {
+    return (
+        <>
+        <Navbar/>
+        <Coviddata/>
+        </>
+    )
+}
+
+export default Home
